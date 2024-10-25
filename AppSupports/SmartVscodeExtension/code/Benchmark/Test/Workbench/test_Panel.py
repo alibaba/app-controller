0b9202dff84935b3375250dd729b094cfdb97916
PengJiazhen408 <513605377@qq.com>
2024-10-25 16:30:50 +0800

    add apis about Panel and fix format bugs about apis

---
+++ AppSupports/SmartVscodeExtension/code/Benchmark/Test/Workbench/test_Panel.py
@@ -0,0 +1,25 @@
+import unittest
+from AppSupports.SmartVscodeExtension.code.Benchmark.Test.BaseTest import BenchmarkTest
+
+class TestWorkbenchPanel(BenchmarkTest):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        test_data_path = 'Workbench/Panel.json'
+        cls.load_tasks(cls, test_data_path)
+
+    def test_1(self):
+        """
+        Set the panel size.
+        """
+        self.evaluate_task()
+        
+    def test_2(self):
+        """
+        Open PROBLEMS panel.
+        """
+        self.evaluate_task()
+        
+
+if __name__ == "__main__":
+    unittest.main()
